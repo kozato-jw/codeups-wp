b8afd0addc6724ccc81f353e84bcccfb6cc2d44b
kozato-jw <kozato.53103@gmail.com>
2025-01-24 00:18:35 +0900

    1/23までの作業（WP/FB修正１回目完了）

--- WordPressTheme/single.php
+++ WordPressTheme/single.php
@@ -28,12 +28,7 @@
               </figure>
               <!-- アイキャッチここまで -->
               <div class="blog-post__content">
-
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <?php the_content() ?>
-                <?php endwhile;
-                endif; ?>
-
+                <?php the_content() ?>
               </div>
             </article>
         <?php endwhile;
